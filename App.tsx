6e90404432b4db1e4f5a9824cf49bed9a649044b
Takahiko Wada <wada.takahiko@gmail.com>
2019-12-30 15:25:49 +0900

    Read text from json and speak it

--- App.tsx
+++ App.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as Speech from "expo-speech";
+import cards from "./data/card.json";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
+
+const App = () => {
+  const [leftCards] = useState<Array<string>>(cards);
+
+  const speechCard = () => {
+    const card = leftCards[0];
+    Speech.speak(card, { language: "JA-jp" });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={speechCard} title="つぎへ" />
+    </View>
+  );
+};
+
+export default App;
